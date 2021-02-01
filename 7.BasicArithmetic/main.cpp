dc5bd9a33e587fb81bba46ad32f5141f28041e6f
Dimitar Mitkov <undeadko@abv.bg>
2021-02-01 17:31:31 +0200

    Lesson 7

---
+++ 7.BasicArithmetic/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 4 + 6;
+
+    cout << x << endl;
+    // Subtraction
+    x = 8 - 4;
+    cout << x << endl;
+    // Multiplication
+    x = 8 * 4;
+    cout << x << endl;
+    // Division
+    x = 81 / 3;
+    cout << x << endl;
+    // Remainder division (Modulus operator)
+    x = 81 % 2;
+    cout << x << endl;
+    // Complex Arithmetic. Brackets count.
+    x = 6 * 4 + (8 * 4 + 9) / 20;
+    cout << x << endl;
+
+    return 0;
+}
