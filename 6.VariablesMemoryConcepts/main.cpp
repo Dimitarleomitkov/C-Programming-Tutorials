27d683712299ca8dc9ce1ea11ab9296063b0a92a
Dimitar Mitkov <undeadko@abv.bg>
2021-02-01 17:31:22 +0200

    Lesson 6

---
+++ 6.VariablesMemoryConcepts/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare and assign a value.
+    int var1 = 50;
+    // Declare here
+    int var2;
+
+    // Assign the value later
+    var2 = 30;
+    // Or change the value at a later time
+    var1 = 20;
+
+    cout << var1 << endl;
+
+    return 0;
+}
