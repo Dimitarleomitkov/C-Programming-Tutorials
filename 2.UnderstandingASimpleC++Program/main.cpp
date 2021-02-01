8c41968d7d8115115b9c42bb3a94f7e760a8e3b0
Dimitar Mitkov <undeadko@abv.bg>
2021-02-01 14:21:00 +0200

    Lesson 2

---
+++ 2.UnderstandingASimpleC++Program/main.cpp
@@ -0,0 +1,20 @@
+// This is a comment
+/* This is a
+   multiple line
+   comment */
+
+// Preprocessor directive
+#include <iostream>
+
+// Namespace
+using namespace std;
+
+// The main function. "{" and "}" indicate the scope of the function.
+int main()
+{
+    // printing stream with a string "Hello world!" in it.
+    // endl is a command for a new line.
+    cout << "Hello world!" << endl;
+    // return 0 indicates the successful execution of the program.
+    return 0;
+}
