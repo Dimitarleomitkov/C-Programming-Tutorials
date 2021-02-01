4565a375b247adc4d461339c4b4062e1127be07a
Dimitar Mitkov <undeadko@abv.bg>
2021-02-01 15:49:29 +0200

    Lesson 4

---
+++ 4.Variables/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Variables are just placeholders.
+    int x = 7;
+
+    // print 7 + 5. Therefore, print 12.
+    cout << x + 5 << endl;
+
+    // Variables can have stupid names.
+    int tuna = 6;
+
+    // Print out the value of the variable - 6.
+    cout << tuna << endl;
+
+    // Basic arithmetic:
+    int a = 4;
+    int b = 21;
+    int sum = a + b;
+
+    // Print out the sum which should be 4 + 21 = 25.
+    cout << sum << endl;
+
+    return 0;
+}
