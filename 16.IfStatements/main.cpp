4f699d94f1791443f6c66b01a317d5914a85efa9
Dimitar Mitkov <undeadko@abv.bg>
2021-02-02 17:27:44 +0200

    Lesson 16

---
+++ 16.IfStatements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+
+    // If statements work with two variables
+    if (x == y) {
+        cout << "OMG, this is broken!" << endl;
+    }
+
+    return 0;
+}
