e0eb63c83f4869ecdd84796aecf718adadeaeb27
Dimitar Mitkov <undeadko@abv.bg>
2021-02-03 10:24:37 +0200

    Lesson 22

---
+++ 22.ForLoops/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Just another type of loop
+    for (int x = 0; x < 10; ++x) {
+        cout << x << endl;
+    }
+
+    return 0;
+}
